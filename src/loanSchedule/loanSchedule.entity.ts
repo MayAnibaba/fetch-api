683926611a02e6713fdd564985de460aefa0ee35
May <97801810+MayAnibaba@users.noreply.github.com>
2023-01-01 20:15:02 +0100

    loan schedule entity

---
+++ src/loanSchedule/loanSchedule.entity.ts
@@ -0,0 +1,36 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('loanSchedule')
+export class LoanSchedule {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    scheduleRef: string;
+
+    @Column()
+    loanId: string;
+
+    @Column({type: 'date'})
+    dueDate: string; 
+
+    @Column({type: 'double'})
+    dueAmount: number; 
+
+    @Column()
+    status: string; 
+
+    @Column({default: false})
+    enablePartialPayment: boolean;
+
+    @Column({default: 'pending'})
+    collectionStatus: string;
+
+    @Column({default: 0, type:'double'})
+    collectedAmount: string;
+
+    @Column({type: 'datetime'})
+    createdAt: string;
+
+}
