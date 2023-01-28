0686ce017e4015cda8f42e4f8812bf905de8c208
May <97801810+MayAnibaba@users.noreply.github.com>
2023-01-28 09:26:36 +0100

    Update ormconfig.ts

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -2,14 +2,26 @@ import { MysqlConnectionOptions } from "typeorm/driver/mysql/MysqlConnectionOpti
 
 const config: MysqlConnectionOptions = {
     type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'machine',
-    password: '7600',
-    database: 'fetch',
+    host: 'containers-us-west-48.railway.app',
+    port: 7472,
+    username: 'root',
+    password: 'ryqskpXqaAMTIARonco1',
+    database: 'railway',
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
     synchronize: true,
     logging: true,
 }
+
+// const config: MysqlConnectionOptions = {
+//     type: 'mysql',
+//     host: 'localhost',
+//     port: 3306,
+//     username: 'machine',
+//     password: '7600',
+//     database: 'fetch',
+//     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//     synchronize: true,
+//     logging: true,
+// }
 
 export default config;
