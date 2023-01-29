9dee25898c48966dd23c71c091d708de230800fc
May <97801810+MayAnibaba@users.noreply.github.com>
2023-01-29 18:11:06 +0100

    changed to mysql2

--- dist/ormconfig.js
+++ dist/ormconfig.js
@@ -2,11 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const config = {
     type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'machine',
-    password: '7600',
-    database: 'fetch',
+    host: 'containers-us-west-48.railway.app',
+    port: 7472,
+    username: 'root',
+    password: 'ryqskpXqaAMTIARonco1',
+    database: 'railway',
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
     synchronize: true,
     logging: true,
