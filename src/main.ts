17f69fae679771b730c4dfe6b0818e9aeafdf191
May <97801810+MayAnibaba@users.noreply.github.com>
2023-01-30 22:38:52 +0100

    added port from env variable

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = 3150;
+  //const PORT = 3150;
+  const PORT = process.env.PORT || 3000;
   await app.listen(PORT, ()=> console.log('server runing on port: '+ PORT));
 }
 bootstrap();
