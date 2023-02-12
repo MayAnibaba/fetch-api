082cb595f91281241eb9be2e7e1bb97a145355a8
May <97801810+MayAnibaba@users.noreply.github.com>
2023-02-12 23:04:18 +0100

    Update TokenData.Module.ts

--- src/tokenData/TokenData.Module.ts
+++ src/tokenData/TokenData.Module.ts
@@ -4,10 +4,11 @@ import { TokenDataEntity } from "./tokenData.entity";
 import { TokenDataService } from "./tokenData.service";
 import { Module } from "@nestjs/common";
 import { LoanEntity } from "src/loan/loan.entity";
+import { LoanService } from "src/loan/loan.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([TokenDataEntity,LoanEntity])],
     controllers: [TokenDataController],
-    providers: [TokenDataService]
+    providers: [TokenDataService,LoanService]
 })
 export class TokenDataModule{}
