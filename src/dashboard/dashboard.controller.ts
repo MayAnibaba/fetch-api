89d770e3275bc23f798070395fc1ff516a9e8343
May <97801810+MayAnibaba@users.noreply.github.com>
2023-02-19 20:35:45 +0100

    Update dashboard.controller.ts

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,6 +1,16 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
+import { get } from "http";
+import { LoanService } from "src/loan/loan.service";
 
 @Controller('dashboard')
 export class DashboardContoller{
+    constructor(private readonly loanService : LoanService,){}
+
+    @Post()
+    async dashboardData() {
+        
+
+    }
+
 
 }
