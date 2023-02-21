55d3d1c3f3e33c6c64ffd91900dac7bb03fc0249
May <97801810+MayAnibaba@users.noreply.github.com>
2023-02-21 10:49:54 +0100

    Update dashboard.module.ts

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -6,10 +6,11 @@ import { TransactionService } from "src/transaction/transaction.service";
 import { LoanService } from "src/loan/loan.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { LoanScheduleEntity } from "src/loanSchedule/loanSchedule.entity";
+import { LoanScheduleService } from "src/loanSchedule/loanSchedule.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([TransactionEntity,LoanEntity,LoanScheduleEntity])],
     controllers: [DashboardContoller],
-    providers: [TransactionService,LoanService]
+    providers: [TransactionService,LoanService,LoanScheduleService]
 })
 export class DashboardModule{}
