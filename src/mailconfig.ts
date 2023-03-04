8585dcf1920a17cacedeae8e7ff8069cdd32592b
May <97801810+MayAnibaba@users.noreply.github.com>
2023-03-04 20:43:19 +0100

    Update mailconfig.ts

--- src/mailconfig.ts
+++ src/mailconfig.ts
@@ -1,22 +1,22 @@
 import * as nodemailer from 'nodemailer';
 
-// const transporter = nodemailer.createTransport({
-//     host: '213.199.131.128',
-//     port: 25,
-//     secure: true,
-//     auth: {
-//         user: 'sofriloans@sofrisofri.com',
-//         pass: '$L0ans@247#'
-//     }
-// });
-
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: '213.199.131.128',
+    port: 25,
+    secure: true,
     auth: {
-        user: 'mmayowa.anibaba@gmail.com',
-        pass: 'gpmvaojblscchfzi'
+        user: 'sofriloans@sofrisofri.com',
+        pass: '$L0ans@247#'
     }
 });
+
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: 'mmayowa.anibaba@gmail.com',
+//         pass: 'gpmvaojblscchfzi'
+//     }
+// });
 
 
 export default transporter
