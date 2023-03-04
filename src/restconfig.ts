29485ffb1861de6c3a19cce2e66162fa9390af2a
May <97801810+MayAnibaba@users.noreply.github.com>
2023-03-04 20:56:09 +0100

    live credentials

--- src/restconfig.ts
+++ src/restconfig.ts
@@ -1,13 +1,23 @@
 
+// const restConfig = {
+//     bankOneUrl: "http://52.168.85.231/BankOneWebAPI/api/",
+//     bankOneAuthToken: "e226bce9-c861-4eab-9a2e-cece85b3615b",
+//     bankOneVersion: "2",
+//     bankOneInsCode: "100618",
+//     webPaystackURl: "http://172.105.152.82/public/index.php/getToken?id=",
+//     paystackURL: "https://api.paystack.co/transaction/charge_authorization",
+//     paystackSecretKey: "sk_test_45213c0b37221dc3a914a01a3ebace00f47f82c2"
+// }
+
+
 const restConfig = {
-    bankOneUrl: "http://52.168.85.231/BankOneWebAPI/api/",
-    bankOneAuthToken: "e226bce9-c861-4eab-9a2e-cece85b3615b",
+    bankOneUrl: "https://api.mybankone.com/BankOneWebAPI/api/",
+    bankOneAuthToken: "c1aaceaa-b1f1-42cf-a2d6-cb8fe02a2fee",
     bankOneVersion: "2",
-    bankOneInsCode: "100618",
+    bankOneInsCode: "100781",
     webPaystackURl: "http://172.105.152.82/public/index.php/getToken?id=",
     paystackURL: "https://api.paystack.co/transaction/charge_authorization",
-    paystackSecretKey: "sk_test_45213c0b37221dc3a914a01a3ebace00f47f82c2"
-
+    paystackSecretKey: "sk_live_caba5bab937b62f7d7f367b0b5ac8951674e9257"
 }
 
 export default restConfig;
