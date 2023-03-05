3e356254dd9646ecd7bffe2fb9b17732864d9dc9
May <97801810+MayAnibaba@users.noreply.github.com>
2023-03-05 10:11:33 +0100

    back to live

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,29 +1,29 @@
 import { MysqlConnectionOptions } from "typeorm/driver/mysql/MysqlConnectionOptions";
 
-const config: MysqlConnectionOptions = {
-    type: 'mysql',
-    host: 'containers-us-west-48.railway.app',
-    port: 7472,
-    username: 'root',
-    password: 'kAQoHNHQcn8OsvccnfJE',
-    database: 'railway',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true,
-    logging: true,
-}
-
-
-
 // const config: MysqlConnectionOptions = {
 //     type: 'mysql',
-//     host: 'localhost',
-//     port: 3306,
-//     username: 'machine',
-//     password: '7600',
-//     database: 'fetch',
+//     host: 'containers-us-west-48.railway.app',
+//     port: 7472,
+//     username: 'root',
+//     password: 'kAQoHNHQcn8OsvccnfJE',
+//     database: 'railway',
 //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
 //     synchronize: true,
 //     logging: true,
 // }
+
+
+
+const config: MysqlConnectionOptions = {
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'machine',
+    password: '7600',
+    database: 'fetch',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+    logging: true,
+}
 
 export default config;
